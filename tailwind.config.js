8037bfa9b0d625d2f5fa77f181d1bfd2dc056a9c
Encore Shao <encore.shao@gmail.com>
2022-09-04 14:12:01 +0800

    feat: adjust css and update avatar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '3xl': '0px 35px 60px -15px rgba(0, 0, 0, 0.9)',
+      }
+    },
   },
   plugins: [],
 }
